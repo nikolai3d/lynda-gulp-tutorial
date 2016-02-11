2ee1fd78fccabf79f3cc8c03ee883688580a7949
Nikolai Svakhin <svakhine@gmail.com>
2016-02-11 03:46:49 +0000

    js task for gulp for script concatenation

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    gcoffee = require("gulp-coffee");
-
+    gcoffee = require("gulp-coffee"),
+    gconcat = require("gulp-concat");
+    
 gulp.task('log', function() {
     gutil.log('works workflows are awesome');
 });
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+    ];
+
 
 gulp.task('coffee', function() {
     
@@ -15,5 +23,17 @@ gulp.task('coffee', function() {
     var destNode = gulp.dest('components/scripts');
     
     srcNode.pipe(processNode).pipe(destNode);
+    
+});
+
+gulp.task('js', function(){
+    var srcNode = gulp.src(jsSources);
+    
+    var concatNode = gconcat('script.js');
+    
+    var destNode = gulp.dest('builds/development/js');
+
+    srcNode.pipe(concatNode).pipe(destNode);
+    
     
 });
