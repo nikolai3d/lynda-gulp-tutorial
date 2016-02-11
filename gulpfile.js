bf15a3d1749aa261200fea606fe79f1db452cb13
Nikolai Svakhin <svakhine@gmail.com>
2016-02-11 03:26:08 +0000

    Gulp coffee step added

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    gcoffee = require("gulp-coffee");
 
 gulp.task('log', function() {
     gutil.log('works workflows are awesome');
 });
 
+
+gulp.task('coffee', function() {
+
+    gulp.src('components/coffee/tagline.coffee')
+        .pipe(gcoffee({
+                bare: true
+            })
+            .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'));
+});
